6dcaf7ae313f607be30362cf2bd6110c68d7efb4
mamun060 <obujraju420@gmail.com>
2022-06-19 18:03:51 +0600

    full crud done express

--- src/Controller/studentsController.js
+++ src/Controller/studentsController.js
@@ -1,5 +1,7 @@
 const studentModel = require('../Models/studuentModel');
 
+
+// Create data 
 const insertStudent = (req, res) => {
     let reqBody = req.body;
 
@@ -12,6 +14,49 @@ const insertStudent = (req, res) => {
     })
 }
 
+// read data
+const readStudentInfo = ( req, res) => {
+    let query = {};
+    let items = "Name Roll Class Remark";
+   studentModel.find(query, items, (error, data)=>{
+        if(error){
+            res.status(400).json({status: "Invalid requrest", error})
+        }else {
+            res.status(201).json({status: 'success', data: data})
+        }
+   }).limit().sort({Roll: 1})
+}
+
+// update data
+const updateStudent = (req, res) => {
+    let id = req.params.id;
+    let query = { _id: id };
+    let updateData = req.body;
+    studentModel.updateOne(query, updateData, (error, data)=>{
+        if(error){
+            res.status(400).json({status: "Invalid requiest, Something wrong", error})
+        } else {
+            res.status(201).json({success: true, data: data})
+        }
+    })
+}
+
+// remove student
+const removeStudent = (req, res) => {
+    const id = req.params.id;
+    let query = { _id: id };
+    studentModel.remove(query, (error, data)=>{
+        if(error){
+            res.status(400).json({status: "Invalid request to remove", error})
+        }else {
+            res.status(201).json({success: true, data: data})
+        }
+    })
+}
+
 module.exports = {
-    insertStudent
+    insertStudent,
+    readStudentInfo,
+    updateStudent,
+    removeStudent
 }
--- src/Routes/api.js
+++ src/Routes/api.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { Hello } = require("../Controller/HelloController");
-const {insertStudent} = require("../Controller/studentsController");
+const {insertStudent, readStudentInfo, updateStudent, removeStudent} = require("../Controller/studentsController");
 
 router.get('/hello', Hello)
 
 
 // mongoose 
 router.post("/insertStudent", insertStudent);
+router.get("/readStudentInfo", readStudentInfo);
+router.post("/updateStudent/:id", updateStudent);
+router.delete("/removeStudent/:id", removeStudent);
 
 module.exports = router;
